a3dc2bf4e2ced5fe36ff87c4f809fe45629ce261
Kraots <nagit.robermessi@gmail.com>
2021-11-25 22:37:57 +0200

    Checks if the id is inside a tuple and not if it's equal to just one id

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -76,7 +76,7 @@ class Moderation(commands.Cog):
         if 913315033134542889 not in (r.id for r in member.roles):
             return await ctx.reply(f'`{member}` is already an admin!')
         admin_role = guild.get_role(913315033134542889)
-        await member.edit(roles=[r for r in member.roles if not r.id == 913315033684008971] + admin_role)
+        await member.edit(roles=[r for r in member.roles if r.id != 913315033684008971] + admin_role)
         await ctx.reply(f'> 👌 Successfully made `{member}` an admin.')
 
     @owner_make.command(name='moderator')
@@ -91,7 +91,7 @@ class Moderation(commands.Cog):
         if 913315033684008971 not in (r.id for r in member.roles):
             return await ctx.reply(f'`{member}` is already a moderator!')
         mod_role = guild.get_role(913315033684008971)
-        await member.edit(roles=[r for r in member.roles if not r.id == 913315033134542889] + mod_role)
+        await member.edit(roles=[r for r in member.roles if r.id != 913315033134542889] + mod_role)
         await ctx.reply(f'> 👌 Successfully made `{member}` a moderator.')
 
     @commands.group(name='remove', invoke_without_command=True, case_insensitive=True, ignore_extra=False)
@@ -111,7 +111,7 @@ class Moderation(commands.Cog):
 
         if 913315033134542889 not in (r.id for r in member.roles):
             return await ctx.reply(f'`{member}` is not a moderator!')
-        await member.edit(roles=[r for r in member.roles if not r.id == 913315033134542889])
+        await member.edit(roles=[r for r in member.roles if r.id not in (913315033134542889, 913315033684008971)])
         await ctx.reply(f'> 👌 Successfully removed `{member}` from being an admin.')
 
     @owner_remove.command(name='moderator')
@@ -124,7 +124,7 @@ class Moderation(commands.Cog):
 
         if 913315033684008971 not in (r.id for r in member.roles):
             return await ctx.reply(f'`{member}` is not a moderator!')
-        await member.edit(roles=[r for r in member.roles if not r.id == 913315033684008971])
+        await member.edit(roles=[r for r in member.roles if r.id not in (913315033134542889, 913315033684008971)])
         await ctx.reply(f'> 👌 Successfully removed `{member}` from being a moderator.')
 
 
