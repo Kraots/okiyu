a0c84a854f38c4af17b1d49ca9946c5347afeebd
Kraots <nagit.robermessi@gmail.com>
2021-11-25 20:45:05 +0200

    Fixed circular import

--- utils/views/verify.py
+++ utils/views/verify.py
@@ -1,7 +1,12 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 import disnake
 from disnake.ui import View, button
 
-from main import Ukiyo
+if TYPE_CHECKING:
+    from main import Ukiyo
 
 
 class Verifiy(View):
