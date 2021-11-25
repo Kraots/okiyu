6fc2a7943b898b49896c457099900ce12418d76e
Kraots <nagit.robermessi@gmail.com>
2021-11-25 22:45:36 +0200

    Fixed wrong operators

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -111,10 +111,10 @@ class Moderation(commands.Cog):
 
         if 913315033134542889 not in (r.id for r in member.roles):
             return await ctx.reply(f'`{member}` is not a moderator!')
-        await member.edit(roles=[r for r in member.roles if r.id == 913315033134542889])
+        await member.edit(roles=[r for r in member.roles if r.id != 913315033134542889])
         await ctx.reply(f'> 👌 Successfully removed `{member}` from being an admin.')
 
-    @owner_remove.command(name='moderator')
+    @owner_remove.command(name='moderator', aliases=('mod',))
     @is_admin()
     async def owner_remove_mod(self, ctx: Context, *, member: disnake.Member):
         """Remove a moderator."""
@@ -124,7 +124,7 @@ class Moderation(commands.Cog):
 
         if 913315033684008971 not in (r.id for r in member.roles):
             return await ctx.reply(f'`{member}` is not a moderator!')
-        await member.edit(roles=[r for r in member.roles if r.id == 913315033684008971])
+        await member.edit(roles=[r for r in member.roles if r.id != 913315033684008971])
         await ctx.reply(f'> 👌 Successfully removed `{member}` from being a moderator.')
 
 
