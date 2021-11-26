3ab6455b59e28dc20fffa56ed180d0ec9dfb50f5
Kraots <nagit.robermessi@gmail.com>
2021-11-26 19:18:37 +0200

    Only check in on_message if the message is in a guild

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -111,7 +111,8 @@ class OnMessage(commands.Cog):
 
     @commands.Cog.listener('on_message')
     async def check_for_invalid_name(self, message: disnake.Message):
-        await utils.check_username(message.author, self.bot)
+        if message.guild:
+            await utils.check_username(message.author, self.bot)
 
 
 def setup(bot: Ukiyo):
