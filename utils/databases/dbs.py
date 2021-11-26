24ff480b270fef4ddd310cd1fd812d13a3f86285
Kraots <nagit.robermessi@gmail.com>
2021-11-26 13:49:06 +0200

    Added some comments, makes things easier to understand

--- utils/databases/dbs.py
+++ utils/databases/dbs.py
@@ -4,8 +4,8 @@ import motor.motor_asyncio
 
 key1 = os.getenv('MONGODBKEY1')
 cluster1 = motor.motor_asyncio.AsyncIOMotorClient(key1)
-database1 = cluster1['Ukiyo']
+database1 = cluster1['Ukiyo']  # Mainly stores intros
 
 key2 = os.getenv('MONGODBKEY2')
 cluster2 = motor.motor_asyncio.AsyncIOMotorClient(key2)
-database2 = cluster2['Ukiyo']
+database2 = cluster2['Ukiyo']  # Mainly stores levels
