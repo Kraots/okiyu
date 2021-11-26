688c929cc7d8ba7e956fb0c02aed3b6a9b0ebd92
Kraots <nagit.robermessi@gmail.com>
2021-11-26 15:48:45 +0200

    Added gender/age/sexuality button roles

--- utils/views/__init__.py
+++ utils/views/__init__.py
@@ -1,3 +1,6 @@
 from .verify import *  # noqa
 from .colour_buttons import *  # noqa
+from .gender_buttons import *  # noqa
+from .age_buttons import *  # noqa
+from .sexuality_buttons import *  # noqa
 from .slash_cmd_roles import *  # noqa
--- utils/views/colour_buttons.py
+++ utils/views/colour_buttons.py
@@ -8,7 +8,7 @@ all_colour_roles = (
     913316660277022730, 913316657550745610, 913316658519609394, 913316660163772477  # The last one is the owner only red
 )
 
-roles = {
+colour_roles = {
     'ukiyo:colour_roles:Illusion': 913316657026437150, 'ukiyo:colour_roles:Black': 913316657844346921,
     'ukiyo:colour_roles:Screaming_Green': 913316658821603328, 'ukiyo:colour_roles:Electric_Violet': 913316657903063041,
     'ukiyo:colour_roles:Red_Orange': 913316659119407114, 'ukiyo:colour_roles:Dodger_Blue': 913316659215880223,
@@ -32,134 +32,134 @@ class ColourButtonRoles(disnake.ui.View):
         super().__init__(timeout=None)
 
     @disnake.ui.button(label='Illusion', custom_id='ukiyo:colour_roles:Illusion', emoji='<:illusion:886669987660574803>')
-    async def Illusion(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Illusion(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Black', custom_id='ukiyo:colour_roles:Black', emoji='<:black:886669987752841216>')
-    async def Black(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Black(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Screaming Green', custom_id='ukiyo:colour_roles:Screaming_Green', emoji='<:screaming_green:886669987769626636>')
-    async def Screaming_Green(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Screaming_Green(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Electric Violet', custom_id='ukiyo:colour_roles:Electric_Violet', emoji='<:electric_violet:886669987798986804>')
-    async def Electric_Violet(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Electric_Violet(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Red Orange', custom_id='ukiyo:colour_roles:Red_Orange', emoji='<:red_orange:886669987798999062>')
-    async def Red_Orange(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Red_Orange(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Dodger Blue', custom_id='ukiyo:colour_roles:Dodger_Blue', emoji='<:dodger_blue:886669987916427294>')
-    async def Dodger_Blue(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Dodger_Blue(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Spring Green', custom_id='ukiyo:colour_roles:Spring_Green', emoji='<:spring_green:886669987924815923>')
-    async def Spring_Green(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Spring_Green(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Madang', custom_id='ukiyo:colour_roles:Madang', emoji='<:madang:886669987991941181>')
-    async def Madang(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Madang(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Perfume', custom_id='ukiyo:colour_roles:Perfume', emoji='<:perfume:886669988008710174>')
-    async def Perfume(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Perfume(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Ice Cold', custom_id='ukiyo:colour_roles:Ice_Cold', emoji='<:ice_cold:886669988008710194>')
-    async def Ice_Cold(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Ice_Cold(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Primrose', custom_id='ukiyo:colour_roles:Primrose', emoji='<:primrose:886669988008718336>')
-    async def Primrose(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Primrose(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Orchid', custom_id='ukiyo:colour_roles:Orchid', emoji='<:orchid:886675375185350696>')
-    async def Orchid(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Orchid(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Mandys Pink', custom_id='ukiyo:colour_roles:Mandys_Pink', emoji='<:mandys_pink:886669988038062154>')
-    async def Mandys_Pink(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Mandys_Pink(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Perano', custom_id='ukiyo:colour_roles:Perano', emoji='<:perano:886669988063227965>')
-    async def Perano(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Perano(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Turquoise', custom_id='ukiyo:colour_roles:Turquoise', emoji='<:turquoise:886669988176470046>')
-    async def Turquoise(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Turquoise(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Wewak', custom_id='ukiyo:colour_roles:Wewak', emoji='<:wewak:886669988214243379>')
-    async def Wewak(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Wewak(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Sunshade', custom_id='ukiyo:colour_roles:Sunshade', emoji='<:sunshade:886669988289720410>')
-    async def Sunshade(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Sunshade(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='White', custom_id='ukiyo:colour_roles:White', emoji='<:white:886669988558176306>')
-    async def White(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def White(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
 
     @disnake.ui.button(label='Broom', custom_id='ukiyo:colour_roles:Broom', emoji='<:broom:886669989636100156>')
-    async def Broom(self, button: disnake.ui.Button, interaction: disnake.Interaction):
+    async def Broom(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
         _roles = [role for role in interaction.author.roles if role.id not in all_colour_roles]
-        _roles.append(interaction.guild.get_role(roles[button.custom_id]))
+        _roles.append(interaction.guild.get_role(all_colour_roles[button.custom_id]))
         await interaction.author.edit(roles=_roles, reason='Colour role update.')
         await interaction.response.send_message(f'I have changed your colour to `{button.label}`', ephemeral=True)
