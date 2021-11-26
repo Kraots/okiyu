a7615755eb7f44c2d4efca15d0fdc7031f4e2d55
Kraots <nagit.robermessi@gmail.com>
2021-11-26 20:12:33 +0200

    Added a bad words filter

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -216,12 +216,13 @@ class Moderation(commands.Cog):
 
         guild = self.bot.get_guild(913310006814859334)
         muted_by = guild.get_member(data.muted_by)
-        if ctx.author.id not in (muted_by, self.bot._owner_id):
-            if muted_by.top_role > ctx.author.top_role:
-                return await ctx.reply(
-                    f'{member.mention} was muted by `{muted_by}` which is in a higher role hierarcy than you. '
-                    'Only staff members of the same role or above can unmute that person.'
-                )
+        if data.bot is False:
+            if ctx.author.id not in (muted_by, self.bot._owner_id):
+                if muted_by.top_role > ctx.author.top_role:
+                    return await ctx.reply(
+                        f'{member.mention} was muted by `{muted_by}` which is in a higher role hierarcy than you. '
+                        'Only staff members of the same role or above can unmute that person.'
+                    )
 
         new_roles = [role for role in member.roles if role.id != 913376647422545951]
         if data.is_owner is True:
--- cogs/on_message.py
+++ cogs/on_message.py
@@ -8,6 +8,31 @@ import utils
 
 from main import Ukiyo
 
+muted_amount_count = {}
+
+
+def get_mute_time(user_id) -> str:
+    try:
+        curr_amount = muted_amount_count[user_id]
+        curr_amount += 1
+        muted_amount_count[user_id] = curr_amount
+    except KeyError:
+        curr_amount = 1
+        muted_amount_count[user_id] = 1
+
+    if curr_amount == 1:
+        return '15 minutes'
+    elif curr_amount == 2:
+        return '30 minutes'
+    elif curr_amount == 3:
+        return '45 minutes'
+    elif curr_amount == 4:
+        return '1 hour'
+    elif curr_amount == 5:
+        return '12 hours'
+    elif curr_amount == 6:
+        return '1 day'
+
 
 async def send_webhook(em: disnake.Embed, view: disnake.ui.View, bot: Ukiyo):
     webhook = await bot.get_webhook(
@@ -20,6 +45,7 @@ async def send_webhook(em: disnake.Embed, view: disnake.ui.View, bot: Ukiyo):
 class OnMessage(commands.Cog):
     def __init__(self, bot: Ukiyo):
         self.bot = bot
+        self.bad_words_filter = {}
 
     @commands.Cog.listener('on_message_delete')
     async def on_message_delete(self, message: disnake.Message):
@@ -110,9 +136,58 @@ class OnMessage(commands.Cog):
                 await cmd.invoke(ctx)
 
     @commands.Cog.listener('on_message')
-    async def check_for_invalid_name(self, message: disnake.Message):
-        if message.guild:
-            await utils.check_username(self.bot, member=message.author)
+    async def on_message(self, message: disnake.Message):
+        if message.author.id != self.bot._owner_id:
+            if message.guild:
+                for word in message.content.split():
+                    if utils.check_bad_word(word) is True:
+                        ctx = await self.bot.get_context(message)
+                        time = get_mute_time(message.author.id)
+                        _data = await utils.UserFriendlyTime(commands.clean_content).convert(ctx, f'{time} [BAD WORD FILTER]')
+                        await message.delete()
+
+                        usr = self.bad_words_filter.get(message.author.id)
+                        if usr is None:
+                            self.bad_words_filter[message.author.id] = 1
+                            usr = self.bad_words_filter[message.author.id]
+                        else:
+                            self.bad_words_filter[message.author.id] += 1
+                        if usr >= 4:
+                            guild = self.bot.get_guild(913310006814859334)
+                            muted_role = guild.get_role(913376647422545951)
+                            data = utils.Mutes(
+                                id=message.author.id,
+                                muted_by=self.bot.user.id,
+                                muted_until=_data.dt,
+                                reason=_data.arg,
+                                bot=True
+                            )
+                            if 913310292505686046 in (r.id for r in message.author.roles):  # Checks for owner
+                                data.is_owner = True
+                            elif 913315033134542889 in (r.id for r in message.author.roles):  # Checks for admin
+                                data.is_admin = True
+                            elif 913315033684008971 in (r.id for r in message.author.roles):  # Checks for mod
+                                data.is_mod = True
+                            await data.commit()
+                            new_roles = [role for role in message.author.roles
+                                         if role.id not in (913310292505686046, 913315033134542889, 913315033684008971)
+                                         ] + [muted_role]
+                            await message.author.edit(roles=new_roles, reason=f'[BAD WORD FILTER]: "{word}"')
+
+                            try:
+                                await message.author.send(
+                                    f'Hello, you have been muted in `Ukiyo` by **{self.bot.user.id}** for **{_data.arg}** '
+                                    f'until {utils.format_dt(_data.dt, "F")} (`{utils.human_timedelta(_data.dt, suffix=False)}`)'
+                                )
+                            except disnake.Forbidden:
+                                pass
+                            await message.channel.send(
+                                f'> ⚠️ **[BAD WORD]** `{message.author}` has been muted for saying a bad word '
+                                f'until {utils.format_dt(_data.dt, "F")} (`{utils.human_timedelta(_data.dt, suffix=False)}`)'
+                            )
+                            return
+
+                await utils.check_username(self.bot, member=message.author)
 
 
 def setup(bot: Ukiyo):
