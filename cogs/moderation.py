f072167f6cb8ac3efd76ca106be11a01dae00fdc
Kraots <nagit.robermessi@gmail.com>
2021-11-26 17:14:51 +0200

    Added "!mute" and "!unmute"

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -1,10 +1,20 @@
-from typing import Union
 import asyncio
+from typing import Union
+from datetime import datetime
 
 import disnake
-from disnake.ext import commands
+from disnake.ext import commands, tasks
 
-from utils import Context, is_mod, is_admin, is_owner
+from utils import (
+    Context,
+    is_mod,
+    is_admin,
+    is_owner,
+    UserFriendlyTime,
+    Mutes,
+    format_dt,
+    human_timedelta
+)
 
 from main import Ukiyo
 
@@ -35,7 +45,7 @@ class Moderation(commands.Cog):
     @commands.command(name='ban')
     @is_admin()
     async def _ban(self, ctx: Context, member: Union[disnake.Member, disnake.User], *, reason: str):
-        """Bans a user."""
+        """Bans an user."""
 
         if ctx.author.top_role <= member.top_role and ctx.author.id != self.bot._owner_id:
             return await ctx.reply('That member is above or equal to you. Cannot do that.')
@@ -44,13 +54,13 @@ class Moderation(commands.Cog):
             await member.send('> Hello! Sadly, you have been **banned** from `Ukiyo`. Goodbye 👋')
         except disnake.Forbidden:
             pass
-        await member.ban(reason=f'{ctx.author} ({ctx.author.id}): {reason}')
+        await member.ban(reason=f'[BAN] {ctx.author} ({ctx.author.id}): {reason}')
         await ctx.send(f'> 👌🔨 Banned {member} for **{reason}**')
 
     @commands.command(name='kick')
     @is_mod()
     async def _kick(self, ctx: Context, member: Union[disnake.Member, disnake.User], *, reason: str):
-        """Kicks a user."""
+        """Kicks a member."""
 
         if ctx.author.top_role <= member.top_role and ctx.author.id != self.bot._owner_id:
             return await ctx.reply('That member is above or equal to you. Cannot do that.')
@@ -59,7 +69,7 @@ class Moderation(commands.Cog):
             await member.send('> Hello! Sadly, you have been **kicked** from `Ukiyo`. Goodbye 👋')
         except disnake.Forbidden:
             pass
-        await member.kick(reason=f'{ctx.author} ({ctx.author.id}): {reason}')
+        await member.kick(reason=f'[KICK] {ctx.author} ({ctx.author.id}): {reason}')
         await ctx.send(f'> 👌 Kicked {member} for **{reason}**')
 
     @commands.group(name='make', invoke_without_command=True, case_insensitive=True, ignore_extra=False)
@@ -131,6 +141,127 @@ class Moderation(commands.Cog):
             return await ctx.reply(f'`{member}` is not a moderator!')
         await member.edit(roles=[r for r in member.roles if r.id != 913315033684008971])
         await ctx.reply(f'> 👌 Successfully removed `{member}` from being a moderator.')
+
+    @commands.command(name='mute')
+    @is_mod()
+    async def mute_cmd(
+        self,
+        ctx: Context,
+        member: disnake.Member,
+        *,
+        time_and_reason: UserFriendlyTime(commands.clean_content)
+    ):
+        """
+        Mute somebody.
+
+        Example:
+        `!mute @carrot 2m coolest person alive`
+        `!mute @carrot coolest person alive 2m`
+        """
+
+        if ctx.author.top_role <= member.top_role and ctx.author.id != self.bot._owner_id:
+            return await ctx.reply('That member is above or equal to you. Cannot do that.')
+
+        is_staff = False
+        time = time_and_reason.dt
+        reason = time_and_reason.arg
+        data = Mutes(
+            id=member.id,
+            muted_by=ctx.author.id,
+            muted_until=time
+        )
+        if 913310292505686046 in member.roles:  # Checks for owner role
+            data.is_owner = True
+            is_staff = True
+        if 913315033134542889 in member.roles:  # Checks for admin role
+            data.is_admin = True
+            is_staff = True
+        if 913315033684008971 in member.roles:  # Checks for mod role
+            data.is_mod = True
+            is_staff = True
+        await data.commit()
+
+        guild = self.bot.get_guild(913310006814859334)
+        muted_role = guild.get_role(913376647422545951)
+        if is_staff is True:
+            new_roles = [role for role in member.roles
+                         if role.id not in (913310292505686046, 913315033134542889, 913315033684008971)
+                         ] + muted_role
+        else:
+            new_roles = [role for role in member.roles
+                         if role.id not in (913310292505686046, 913315033134542889, 913315033684008971)]
+
+        await member.edit(roles=new_roles, reason=f'[MUTE] {ctx.author} ({ctx.author.id}): {reason}')
+        try:
+            await member.send(
+                f'Hello, you have been muted in `Ukiyo` by **{ctx.author}** for **{reason}** '
+                f'until {format_dt(time, "D")} (`{human_timedelta(time, suffix=False)}`)'
+            )
+        except disnake.Forbidden:
+            pass
+        await ctx.reply(
+            f'> 👌📨 Applied mute to {member.mention} for **{reason}** '
+            f'until {format_dt(time, "D")} (`{human_timedelta(time, suffix=False)}`)'
+        )
+
+    @commands.command(name='unmute')
+    async def unmute_cmd(self, ctx: Context, *, member: disnake.Member):
+        """Unmute somebody that is currently muted."""
+
+        data: Mutes = await Mutes.find_one({'_id': member.id})
+        if not data:
+            return await ctx.reply(f'`{member}` is not muted!')
+        await data.delete()
+
+        guild = self.bot.get_guild(913310006814859334)
+        new_roles = [role for role in member.roles if not role.id == 913376647422545951]
+        if data.is_owner is True:
+            owner_role = guild.get_role(913310292505686046)
+            new_roles += [owner_role]
+        elif data.is_admin is True:
+            admin_role = guild.get_role(913315033134542889)
+            new_roles += [admin_role]
+        elif data.is_mod is True:
+            mod_role = guild.get_role(913315033684008971)
+            new_roles += [mod_role]
+        await member.edit(roles=new_roles, reason=f'[UNMUTE] Unmuted by {ctx.author} ({ctx.author.id})')
+        try:
+            await member.send(f'Hello, you have been **unmuted** in `Ukiyo` by {ctx.author}')
+        except disnake.Forbidden:
+            pass
+
+        await ctx.reply(f'> 👌 Successfully unmuted {member.mention}')
+
+    @mute_cmd.error
+    async def mute_cmd_error(self, ctx: Context, error):
+        if isinstance(error, commands.BadArgument):
+            return await ctx.reply(error)
+        await self.bot.reraise(ctx, error)
+
+    @tasks.loop(seconds=15.0)
+    async def check_unmutes(self):
+        mutes: list[Mutes] = await Mutes.find().sort('muted_until', 1).to_list(5)
+        for mute in mutes:
+            if datetime.utcnow() >= mute.muted_until:
+                guild = self.bot.get_guild(913310006814859334)
+                member = guild.get_member(mute.id)
+                if member:
+                    new_roles = [role for role in member.roles if not role.id == 913376647422545951]
+                    if mute.is_owner is True:
+                        owner_role = guild.get_role(913310292505686046)
+                        new_roles += [owner_role]
+                    elif mute.is_admin is True:
+                        admin_role = guild.get_role(913315033134542889)
+                        new_roles += [admin_role]
+                    elif mute.is_mod is True:
+                        mod_role = guild.get_role(913315033684008971)
+                        new_roles += [mod_role]
+                    await member.edit(roles=new_roles, reason='[UNMUTE] Mute Expired.')
+                    try:
+                        await member.send('Hello, your mute in `Ukiyo` has expired. You have been unmuted.')
+                    except disnake.Forbidden:
+                        pass
+                await mute.delete()
 
 
 def setup(bot: Ukiyo):
---
+++ utils/databases/db_mutes.py
@@ -0,0 +1,21 @@
+from . import database2
+
+from umongo.fields import *
+from umongo.frameworks.motor_asyncio import MotorAsyncIOInstance as Instance
+from umongo.frameworks.motor_asyncio import MotorAsyncIODocument as Document
+
+instance = Instance(database2)
+
+
+@instance.register
+class Mutes(Document):
+    id = IntField(attribute='_id', required=True)
+    muted_by = IntField(required=True)
+    muted_until = DateTimeField(required=True)
+
+    is_owner = BooleanField(default=False)
+    is_admin = BooleanField(default=False)
+    is_mod = BooleanField(default=False)
+
+    class Meta:
+        collection_name = 'Mutes'
