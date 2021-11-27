43cbc1d59c99c49079c63b1d36882ace2d13fe9e
Kraots <nagit.robermessi@gmail.com>
2021-11-27 10:11:32 +0200

    Added "!announce"

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import re
 import string
 import asyncio
 import functools
@@ -24,6 +25,7 @@ __all__ = (
     'check_bad_word',
     'check_username',
     'run_in_executor',
+    'clean_inter_content',
 )
 
 allowed_letters = tuple(list(string.ascii_lowercase) + list(string.digits) + list(string.punctuation) + ['♡', ' ', '\\'])
@@ -291,3 +293,60 @@ def run_in_executor(func: Callable):
         return await loop.run_in_executor(None, to_run)
 
     return wrapper
+
+
+def clean_inter_content(
+    *,
+    fix_channel_mentions: bool = False,
+    use_nicknames: bool = True,
+    escape_markdown: bool = False,
+    remove_markdown: bool = False,
+):
+    async def convert(inter: disnake.ApplicationCommandInteraction, argument: str):
+        if inter.guild:
+            def resolve_member(id: int) -> str:
+                m = inter.guild.get_member(id)
+                return f'@{m.display_name if use_nicknames else m.name}' if m else '@deleted-user'
+
+            def resolve_role(id: int) -> str:
+                r = inter.guild.get_role(id)
+                return f'@{r.name}' if r else '@deleted-role'
+        else:
+            def resolve_member(id: int) -> str:
+                m = inter.bot.get_user(id)
+                return f'@{m.name}' if m else '@deleted-user'
+
+            def resolve_role(id: int) -> str:
+                return '@deleted-role'
+
+        if fix_channel_mentions and inter.guild:
+            def resolve_channel(id: int) -> str:
+                c = inter.guild.get_channel(id)
+                return f'#{c.name}' if c else '#deleted-channel'
+        else:
+            def resolve_channel(id: int) -> str:
+                return f'<#{id}>'
+
+        transforms = {
+            '@': resolve_member,
+            '@!': resolve_member,
+            '#': resolve_channel,
+            '@&': resolve_role,
+        }
+
+        def repl(match: re.Match) -> str:
+            type = match[1]
+            id = int(match[2])
+            transformed = transforms[type](id)
+            return transformed
+
+        result = re.sub(r'<(@[!&]?|#)([0-9]{15,20})>', repl, argument)
+        if escape_markdown:
+            result = disnake.utils.escape_markdown(result)
+        elif remove_markdown:
+            result = disnake.utils.remove_markdown(result)
+
+        # Completely ensure no mentions escape:
+        return disnake.utils.escape_mentions(result)
+
+    return convert
