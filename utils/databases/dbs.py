b493f4eaf11e401d930c26fc565e1986a9328789
Kraots <nagit.robermessi@gmail.com>
2021-12-05 11:53:35 +0200

    added "!afk"

--- utils/databases/dbs.py
+++ utils/databases/dbs.py
@@ -13,3 +13,11 @@ database2 = cluster2['Ukiyo']  # Mainly stores levels
 key3 = os.getenv('MONGODBKEY3')
 cluster3 = motor.motor_asyncio.AsyncIOMotorClient(key3)
 database3 = cluster3['Ukiyo']  # Extra stuff (mutes, rules, etc...)
+
+key4 = os.getenv('MONGODBKEY4')
+cluster4 = motor.motor_asyncio.AsyncIOMotorClient(key4)
+database4 = cluster4['Ukiyo2']  # AFKs
+
+# key5 = os.getenv('MONGODBKEY5')
+# cluster5 = motor.motor_asyncio.AsyncIOMotorClient(key5)
+# database5 = cluster5['Ukiyo2']  # Marriages
