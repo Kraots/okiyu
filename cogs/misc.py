fc002d98928d4f06beefbcd5bc615dceb3507beb
Kraots <nagit.robermessi@gmail.com>
2021-12-06 23:02:40 +0200

    bettered "!nick" response

--- cogs/misc.py
+++ cogs/misc.py
@@ -223,7 +223,10 @@ class Misc(commands.Cog):
 
         res = await utils.check_username(self.bot, word=new_nickname)
         if res is True:
-            return await ctx.reply(f'> {ctx.disagree} That nickname is not pingable, is a bad word or is too short!')
+            return await ctx.reply(
+                f'> {ctx.disagree} Cannot remove your nickname because your username '
+                'has too less pingable characters, is a bad word or is too short.'
+            )
         await ctx.author.edit(nick=new_nickname)
         await ctx.reply(f'I have changed your nickname to `{new_nickname}`')
 
@@ -235,7 +238,7 @@ class Misc(commands.Cog):
         if res is True:
             return await ctx.reply(
                 f'> {ctx.disagree} Cannot remove your nickname because your username '
-                'is unpingable, is a bad word or is too short.'
+                'has too less pingable characters, is a bad word or is too short.'
             )
         await ctx.author.edit(nick=None)
         await ctx.reply('I have removed your nickname.')
