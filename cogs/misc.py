26695fb1554dc6543a0f01916905c990f8fa583b
Kraots <70799557+Kraots@users.noreply.github.com>
2021-12-07 23:28:29 +0200

    Fix typo

--- cogs/misc.py
+++ cogs/misc.py
@@ -224,7 +224,7 @@ class Misc(commands.Cog):
         res = await utils.check_username(self.bot, word=new_nickname)
         if res is True:
             return await ctx.reply(
-                f'> {ctx.disagree} Cannot remove your nickname because your username '
+                f'> {ctx.disagree} Cannot change your nickname because your username '
                 'has too less pingable characters, is a bad word or is too short.'
             )
         await ctx.author.edit(nick=new_nickname)
