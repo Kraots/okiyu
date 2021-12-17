aa7a75d4c8da535792832b76a8bf2b21d85c37a6
Kraots <nagit.robermessi@gmail.com>
2021-12-17 23:54:32 +0200

    forgot to add the ``game`` prefix to ``!shop`` in the ``!game characters``

--- cogs/game.py
+++ cogs/game.py
@@ -129,7 +129,7 @@ class _Game(commands.Cog, name='Game'):
         if not data.characters:
             em = disnake.Embed(
                 title='Uh-oh',
-                description='You do not own any characters! See `!shop` to check all the available buyable characters.',
+                description='You do not own any characters! See `!game shop` to check all the available buyable characters.',
                 color=utils.red
             )
             await ctx.reply(embed=em)
