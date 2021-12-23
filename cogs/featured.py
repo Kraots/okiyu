2c30f82788ec4f74e65566e4d9ce5b32b611cf81
Kraots <nagit.robermessi@gmail.com>
2021-12-23 21:28:32 +0200

    fix the docstring for timediff

--- cogs/featured.py
+++ cogs/featured.py
@@ -20,18 +20,18 @@ class Featured(commands.Cog):
         return '💡'
 
     @commands.command(name='timediff')
-    async def time_difference(self, ctx: Context, time1: str, time2: str):
+    async def time_difference(self, ctx: Context, date1: str, date2: str):
         """Compare 2 dates. The format in which you give this must be **day/month/year**.
 
-        `time1` **->** The greater time you want to compare time2 to. That means that this must be younger(later) than **time2**.
-        `time2` **->** The earliest time you want to compare time1 to. That means that this must be the older(earlier) than **time1**
+        `time1` **->** The first date you want to compare the second date to.
+        `time2` **->** The second date you want to compare the first date to.
 
         **Example:**
         `!timediff 24/08/2005 23/12/2021` **->** This will show the exact difference (not hour/sec precise) between 23rd of December 2021 and 25th of August 2005 (also the owner's birthday :flushed:)
         """  # noqa
 
-        time1: datetime = datetime.strptime(time1, '%d/%m/%Y')
-        time2: datetime = datetime.strptime(time2, '%d/%m/%Y')
+        time1 = datetime.strptime(date1, '%d/%m/%Y')
+        time2 = datetime.strptime(date2, '%d/%m/%Y')
         diff = utils.human_timedelta(time2, source=time1, suffix=False, accuracy=4)
 
         _time1 = time1.strftime('%d %B %Y')
