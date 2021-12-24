465646cb146ae524ea6323272948734b8e9dab23
Kraots <nagit.robermessi@gmail.com>
2021-12-24 23:34:15 +0200

    decreased the max amount for blackjack to 5k

--- cogs/game.py
+++ cogs/game.py
@@ -806,7 +806,7 @@ class _Game(commands.Cog, name='Game'):
         amount = amount.lower()
         data = await self.get_user(ctx.author.id)
         if amount == 'all':
-            amount = 10000 if data.coins >= 10000 else data.coins
+            amount = 5000 if data.coins >= 5000 else data.coins
         else:
             amount = utils.format_amount(amount)
             try:
@@ -821,8 +821,8 @@ class _Game(commands.Cog, name='Game'):
         elif amount > data.coins:
             em = utils.fail_embed('You don\'t have that many coins.')
             return await ctx.reply(embed=em)
-        elif amount > 10000:
-            em = utils.fail_embed(f'You can\'t bet more than **10,000** {self.coin_emoji}')
+        elif amount > 5000:
+            em = utils.fail_embed(f'You can\'t bet more than **5,000** {self.coin_emoji}')
             return await ctx.reply(embed=em)
 
         view = utils.BlackJack(ctx.author, self.bot, amount)
