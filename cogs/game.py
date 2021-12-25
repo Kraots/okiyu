fe139fd7529e36f73b8eb7e382f24f22a6824c87
Kraots <70799557+Kraots@users.noreply.github.com>
2021-12-25 13:57:47 +0200

    Decreased the cap to 5k coins blackjack

--- cogs/game.py
+++ cogs/game.py
@@ -811,7 +811,7 @@ class _Game(commands.Cog, name='Game'):
         amount = amount.lower()
         data = await self.get_user(ctx.author.id)
         if amount == 'all':
-            amount = 30000 if data.coins >= 30000 else data.coins
+            amount = 5000 if data.coins >= 5000 else data.coins
         else:
             amount = utils.format_amount(amount)
             try:
@@ -826,8 +826,8 @@ class _Game(commands.Cog, name='Game'):
         elif amount > data.coins:
             em = utils.fail_embed('You don\'t have that many coins.')
             return await ctx.reply(embed=em)
-        elif amount > 30000:
-            em = utils.fail_embed(f'You can\'t bet more than **30,000** {self.coin_emoji}')
+        elif amount > 5000:
+            em = utils.fail_embed(f'You can\'t bet more than **5,000** {self.coin_emoji}')
             return await ctx.reply(embed=em)
 
         view = utils.BlackJack(ctx.author, self.bot, amount)
