4a49bde6fd2b8a0a16bb0a4b22fca24dc2a81234
Kraots <70799557+Kraots@users.noreply.github.com>
2022-01-06 21:52:50 +0200

    moved the bad words and invite filter to automod

--- cogs/on_message.py
+++ cogs/on_message.py
@@ -1,4 +1,3 @@
-import re
 import asyncio
 import datetime
 
@@ -9,193 +8,10 @@ import utils
 
 from main import Ukiyo
 
-muted_amount_count = {}
-
-
-def get_mute_time(user_id) -> str:
-    try:
-        curr_amount = muted_amount_count[user_id]
-        curr_amount += 1
-        muted_amount_count[user_id] = curr_amount
-    except KeyError:
-        curr_amount = 1
-        muted_amount_count[user_id] = 1
-
-    if curr_amount == 1:
-        return '15 minutes'
-    elif curr_amount == 2:
-        return '30 minutes'
-    elif curr_amount == 3:
-        return '45 minutes'
-    elif curr_amount == 4:
-        return '1 hour'
-    elif curr_amount == 5:
-        return '12 hours'
-    elif curr_amount == 6:
-        return '1 day'
-    else:
-        return '1 month'  # This shouldn't really even happen, but just in case it really gets that bad.
-
 
 class OnMessage(commands.Cog):
     def __init__(self, bot: Ukiyo):
         self.bot = bot
-        self.bad_words_filter = {}
-
-    async def check_bad_word(self, message: disnake.Message):
-        if 913310292505686046 in (r.id for r in message.author.roles):  # Checks for owner
-            return
-        guild = self.bot.get_guild(913310006814859334)
-        if message.content is not None:
-            if utils.check_profanity(message.content) is True:
-                ctx = await self.bot.get_context(message, cls=utils.Context)
-                await message.delete()
-
-                _ = self.bad_words_filter.get(message.author.id)
-                if _ is None:
-                    self.bad_words_filter[message.author.id] = 1
-                else:
-                    self.bad_words_filter[message.author.id] += 1
-                if self.bad_words_filter[message.author.id] >= 4:
-                    self.bad_words_filter[message.author.id] = 0
-                    time = get_mute_time(message.author.id)
-                    _data = await utils.UserFriendlyTime(commands.clean_content).convert(ctx, f'{time} Bad Words.')
-                    muted_role = guild.get_role(913376647422545951)
-                    duration = utils.human_timedelta(_data.dt, suffix=False)
-                    data = utils.Mutes(
-                        id=message.author.id,
-                        muted_by=self.bot.user.id,
-                        muted_until=_data.dt,
-                        reason=_data.arg,
-                        duration=duration,
-                        filter=True,
-                        muted=True
-                    )
-                    if 913315033134542889 in (r.id for r in message.author.roles):  # Checks for admin
-                        data.is_admin = True
-                    elif 913315033684008971 in (r.id for r in message.author.roles):  # Checks for mod
-                        data.is_mod = True
-                    new_roles = [role for role in message.author.roles
-                                 if role.id not in (913310292505686046, 913315033134542889, 913315033684008971)
-                                 ] + [muted_role]
-                    await message.author.edit(roles=new_roles, reason='[BAD WORD FILTER]')
-
-                    try:
-                        em = disnake.Embed(title='You have been muted!', color=utils.red)
-                        em.description = f'**Muted By:** {self.bot.user}\n' \
-                                         f'**Reason:** {_data.arg}\n' \
-                                         f'**Mute Duration:** `{duration}`\n' \
-                                         f'**Expire Date:** {utils.format_dt(_data.dt, "F")}' \
-                                         f'**Remaining:** {utils.human_timedelta(data.muted_until, suffix=False, accuracy=6)}'
-                        em.set_footer(text='Muted in `Ukiyo`')
-                        em.timestamp = datetime.datetime.now(datetime.timezone.utc)
-                        await message.author.send(embed=em)
-                    except disnake.Forbidden:
-                        pass
-                    _msg = await message.channel.send(
-                        f'> ⚠️ **[BAD WORD]** {message.author.mention} has been muted for saying a bad word '
-                        f'until {utils.format_dt(_data.dt, "F")} (`{duration}`)'
-                    )
-                    data.jump_url = _msg.jump_url
-                    await data.commit()
-                    view = disnake.ui.View()
-                    view.add_item(disnake.ui.Button(label='Jump!', url=_msg.jump_url))
-                    await utils.log(
-                        self.bot.webhooks['mod_logs'],
-                        title='[MUTE]',
-                        fields=[
-                            ('Member', f'{message.author} (`{message.author.id}`)'),
-                            ('Reason', 'Bad Words.'),
-                            ('Mute Duration', f'`{duration}`'),
-                            ('Expires At', utils.format_dt(_data.dt, "F")),
-                            ('Remaining', f'`{utils.human_timedelta(data.muted_until, suffix=False, accuracy=6)}`'),
-                            ('By', f'{self.bot.user.mention} (`{self.bot.user.id}`)'),
-                            ('At', utils.format_dt(datetime.datetime.now(), 'F')),
-                        ],
-                        view=view
-                    )
-
-    async def check_invite(self, message: disnake.Message):
-        guild = self.bot.get_guild(913310006814859334)
-        matches = re.findall(utils.invite_regex, message.content.lower())
-        if matches and message.author.id != self.bot._owner:
-            if 913310292505686046 in (r.id for r in message.author.roles):  # Checks for owner
-                return
-            elif 913315033134542889 in (r.id for r in message.author.roles):  # Checks for admin
-                return
-
-            if len(matches) == 1:
-                _inv = matches[0].split('/')
-                inv = _inv[-1]
-                if any(invite for invite in await guild.invites() if invite.code.lower() == inv):
-                    return
-
-            ctx = await self.bot.get_context(message, cls=utils.Context)
-            await message.delete()
-            _data = await utils.UserFriendlyTime(commands.clean_content).convert(
-                ctx, '30 minutes Invite that isn\'t towards ``Ukiyo`` found.'
-            )
-            muted_role = guild.get_role(913376647422545951)
-            duration = utils.human_timedelta(_data.dt, suffix=False)
-            data = utils.Mutes(
-                id=message.author.id,
-                muted_by=self.bot.user.id,
-                muted_until=_data.dt,
-                reason=_data.arg,
-                duration=duration,
-                filter=True,
-                muted=True
-            )
-            if 913315033684008971 in (r.id for r in message.author.roles):  # Checks for mod
-                data.is_mod = True
-            new_roles = [role for role in message.author.roles
-                         if role.id != 913315033684008971
-                         ] + [muted_role]
-            await message.author.edit(roles=new_roles, reason='[INVITE FILTER]')
-
-            try:
-                em = disnake.Embed(title='You have been muted!', color=utils.red)
-                em.description = f'**Muted By:** {self.bot.user}\n' \
-                                 f'**Reason:** {_data.arg}\n' \
-                                 f'**Mute Duration:** `{duration}`\n' \
-                                 f'**Expire Date:** {utils.format_dt(_data.dt, "F")}'
-                em.set_footer(text='Muted in `Ukiyo`')
-                em.timestamp = datetime.datetime.now(datetime.timezone.utc)
-                await message.author.send(embed=em)
-            except disnake.Forbidden:
-                pass
-            _msg = await message.channel.send(
-                f'> ⚠️ **[INVITE]** {message.author.mention} has been muted for sending an invite '
-                f'until {utils.format_dt(_data.dt, "F")} (`{duration}`)'
-            )
-            data.jump_url = _msg.jump_url
-            await data.commit()
-            view = disnake.ui.View()
-            view.add_item(disnake.ui.Button(label='Jump!', url=_msg.jump_url))
-
-            invite_logs = guild.get_channel(913332511789178951)
-            em = disnake.Embed(
-                title='New Invite Found!!',
-                description=f'`{message.author}` sent an invite in {message.channel.mention}'
-            )
-            em.set_footer(text=f'User ID: {message.author.id}')
-            v = disnake.ui.View()
-            v.add_item(disnake.ui.Button(label='Jump!', url=message.jump_url))
-            await invite_logs.send(embed=em, view=v)
-
-            await utils.log(
-                self.bot.webhooks['mod_logs'],
-                title='[MUTE]',
-                fields=[
-                    ('Member', f'{message.author} (`{message.author.id}`)'),
-                    ('Reason', 'Invite Sent.'),
-                    ('Mute Duration', f'`{duration}`'),
-                    ('Expires At', utils.format_dt(_data.dt, "F")),
-                    ('By', f'{self.bot.user.mention} (`{self.bot.user.id}`)'),
-                    ('At', utils.format_dt(datetime.datetime.now(), 'F')),
-                ],
-                view=view
-            )
 
     @commands.Cog.listener('on_message_delete')
     async def on_message_delete(self, message: disnake.Message):
@@ -261,8 +77,6 @@ class OnMessage(commands.Cog):
                     inline=False
                 )
 
-            await self.check_bad_word(after)
-            await self.check_invite(after)
             await utils.check_username(after.author)
             await asyncio.sleep(0.5)
             try:
@@ -279,8 +93,6 @@ class OnMessage(commands.Cog):
             return
         if message.author.id != self.bot._owner_id:
             if message.guild and message.content != '':
-                await self.check_bad_word(message)
-                await self.check_invite(message)
                 await utils.check_username(message.author)
 
     @commands.Cog.listener('on_message_edit')
