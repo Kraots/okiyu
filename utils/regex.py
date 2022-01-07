5b6fb5bf88ebddf9b09ee434af8b6d1b8fd25291
Kraots <70799557+Kraots@users.noreply.github.com>
2022-01-07 23:38:57 +0200

    added TOKEN_REGEX

--- utils/regex.py
+++ utils/regex.py
@@ -3,7 +3,9 @@ import re
 __all__ = (
     'INVITE_REGEX',
     'LANGUAGE_REGEX',
+    'TOKEN_REGEX',
 )
 
 INVITE_REGEX = re.compile(r"(?:https?://)?discord(?:(?:app)?\.com/invite|\.gg)/?[a-zA-Z0-9]+/?")
 LANGUAGE_REGEX = re.compile(r"(\w*)\s*(?:```)(\w*)?([\s\S]*)(?:```$)")
+TOKEN_REGEX = re.compile(r'[a-zA-Z0-9_-]{23,28}\.[a-zA-Z0-9_-]{6,7}\.[a-zA-Z0-9_-]{27}')
