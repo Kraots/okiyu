d1554563ed2b36258ed5c69da4bfb279761c6f73
Kraots <70799557+Kraots@users.noreply.github.com>
2022-01-10 22:57:27 +0200

    better invite regex

--- utils/regex.py
+++ utils/regex.py
@@ -6,6 +6,15 @@ __all__ = (
     'TOKEN_REGEX',
 )
 
-INVITE_REGEX = re.compile(r"(?:https?://)?discord(?:(?:app)?\.com/invite|\.gg)/?[a-zA-Z0-9]+/?")
+INVITE_REGEX = re.compile(
+    r"(?:discord(?:[\.,]|dot)gg|"  # Could be discord.gg/
+    r"discord(?:[\.,]|dot)com(?:\/|slash)invite|"  # or discord.com/invite/
+    r"discordapp(?:[\.,]|dot)com(?:\/|slash)invite|"  # or discordapp.com/invite/
+    r"discord(?:[\.,]|dot)me|"  # or discord.me
+    r"discord(?:[\.,]|dot)io"  # or discord.io.
+    r")(?:[\/]|slash)"  # / or 'slash'
+    r"([a-zA-Z0-9\-]+)",  # the invite code itself
+    flags=re.IGNORECASE,
+)
 LANGUAGE_REGEX = re.compile(r"(\w*)\s*(?:```)(\w*)?([\s\S]*)(?:```$)")
 TOKEN_REGEX = re.compile(r'[a-zA-Z0-9_-]{23,28}\.[a-zA-Z0-9_-]{6,7}\.[a-zA-Z0-9_-]{27}')
