0ebb36f9a8643589fc0881aee33f255919c1eaa6
Kraots <70799557+Kraots@users.noreply.github.com>
2022-01-16 13:24:56 +0200

    added ``check_marriage``

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -11,6 +11,67 @@ class Nsfw(commands.Cog):
     def __init__(self, bot: Ukiyo):
         self.bot = bot
 
+    @staticmethod
+    async def check_marriage(
+        command_name: str,
+        ctx: Context,
+        member: disnake.Member
+    ):
+        """
+        Checks whether the ``author`` is married to the ``member`` or not.
+
+        If one of them is married, it will return ``False``, if none of them are married or
+        author is married to the member they're trying to do a nsfw command on, returns ``True``.
+
+        Parameters
+        ----------
+            command_name: :class:`str`
+                The name of the command invoked. This is used when sending the feedback
+                letting the author know they cannot do that action with that member
+                because of specific marriage reasons.
+
+            ctx: :class:`.Context`
+                The context object to get the author from.
+
+            member: :class:`.Member`
+                The member to compare the context's author with.
+
+        Return
+        ------
+            ``True`` | ``False``
+        """
+
+        author_entry: utils.Marriage = await utils.Marriage.get(ctx.author.id)
+        member_entry: utils.Marriage = await utils.Marriage.get(member.id)
+
+        if (
+            (author_entry is None or author_entry.married_to == 0) and
+            (member_entry is None or member_entry.married_to == 0)
+        ):
+            return True
+
+        elif author_entry.married_to != 0 and author_entry.married_to != member.id:
+            mem = ctx.ukiyo.get_member(author_entry.married_to)
+            await mem.send(
+                f'`{utils.format_name(ctx.author)}` has tried to cheat on you.\n'
+                f'They wanted to **{command_name}** {member.mention} ({utils.format_name(member)})'
+            )
+            await ctx.reply(
+                f'{ctx.denial} You cannot **{command_name}** `{utils.format_name(member)}`, '
+                f'you can only **{command_name}** {mem.mention}\nYour partner has been notified '
+                'about your cheating behaviour.'
+            )
+            return False
+
+        elif member_entry != 0 and member_entry.married_to != ctx.author.id:
+            await ctx.reply(
+                f'{ctx.denial} You cannot **{command_name}** that person '
+                'because they are married with somebody else!'
+            )
+            return False
+
+        return True
+
     @commands.group(name='nsfw', invoke_without_command=True, case_insensitive=True, hidden=True)
     async def base_nsfw(self, ctx: Context):
         """Base command for all nsfw commands."""
