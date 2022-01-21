ca677d7dbd8de33b349bd1a897571b58ea0770ae
Kraots <70799557+Kraots@users.noreply.github.com>
2022-01-21 20:11:56 +0200

    added enumerated bool kwarg

--- utils/views/select.py
+++ utils/views/select.py
@@ -6,13 +6,18 @@ __all__ = (
 
 
 class Select(disnake.ui.Select['SelectView']):
-    def __init__(self, options: list[str]):
+    def __init__(self, options: list[str], *, enumerated: bool = False):
         super().__init__(placeholder='Select one of the options...', min_values=1, max_values=1)
+        self.enumerated = enumerated
         self._fill_options(options)
 
     def _fill_options(self, options: list[str]):
-        for option in options:
-            self.add_option(label=option)
+        if self.enumerated is False:
+            for option in options:
+                self.add_option(label=option)
+        else:
+            for index, option in enumerate(options):
+                self.add_option(label=f'{index + 1}. {option}', value=option)
 
     async def callback(self, interaction: disnake.MessageInteraction):
         assert self.view is not None
@@ -25,9 +30,9 @@ class SelectView(disnake.ui.View):
     message: disnake.Message
     value: str | int | bool | float = None
 
-    def __init__(self, options: list[str], *, timeout: float = 30.0):
+    def __init__(self, options: list[str], *, timeout: float = 30.0, enumerated: bool = False):
         super().__init__(timeout=timeout)
-        self.add_item(Select(options))
+        self.add_item(Select(options, enumerated=enumerated))
 
     async def on_timeout(self):
         for item in self.children:
