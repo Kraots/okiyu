b4817dd53255b8af2bc86e9f6ac1c64bc60a8717
Kraots <70799557+Kraots@users.noreply.github.com>
2022-01-21 23:56:54 +0200

    added ``welcome_webhook`` to the webhooks

--- main.py
+++ main.py
@@ -122,9 +122,14 @@ class Ukiyo(commands.Bot):
                 self.get_channel(913332431417925634),
                 avatar=av
             )
+            welcome_webhook = await self.get_webhook(
+                self.get_channel(913331535170637825),
+                avatar=av
+            )
             self.webhooks['logs'] = logs
             self.webhooks['mod_logs'] = mod_logs
             self.webhooks['message_logs'] = message_logs
+            self.webhooks['welcome_webhook'] = welcome_webhook
 
         print('Bot is ready!')
 
