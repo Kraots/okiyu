1a6dafc1abb4ade828c9eaf61df33be159d957e1
Kraots <70799557+Kraots@users.noreply.github.com>
2022-01-21 22:00:44 +0200

    select view has interaction check now

--- utils/views/select.py
+++ utils/views/select.py
@@ -1,5 +1,7 @@
 import disnake
 
+from ..context import Context
+
 __all__ = (
     'SelectView',
 )
@@ -32,9 +34,26 @@ class SelectView(disnake.ui.View):
     message: disnake.Message
     value: str | int | bool | float = None
 
-    def __init__(self, options: list[str], *, timeout: float = 30.0, enumerated: bool = False):
+    def __init__(
+        self,
+        options: list[str],
+        ctx: Context,
+        *,
+        timeout: float = 30.0,
+        enumerated: bool = False
+    ):
         super().__init__(timeout=timeout)
         self.add_item(Select(options, enumerated=enumerated))
+        self.ctx = ctx
+
+    async def interaction_check(self, inter: disnake.MessageInteraction) -> bool:
+        if inter.user and inter.user.id in (self.ctx.bot._owner_id, self.ctx.author.id):
+            return True
+        await inter.response.send_message(
+            'This pagination menu cannot be controlled by you, sorry!',
+            ephemeral=True
+        )
+        return False
 
     async def on_timeout(self):
         for item in self.children:
